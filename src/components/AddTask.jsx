babba9d9a9f6b7a6c24678eaa681db99ca648c2e
Yuri Romeu <yuriromeu2007@gmail.com>
2025-07-23 09:09:20 -0300

    adicionar tarefa e router de descrição

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,36 @@
-function AddTask() {
+import { useState } from 'react';
+function AddTask({ onAddTaskSubmit }) {
+     const [title, setTitle] = useState('');
+     const [description, setDescription] = useState('');
+
      return (
-          <div>
-               <h1>Add Task</h1>
+          <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+               <input
+                    type="text"
+                    placeholder="Enter the task title"
+                    className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+               />
+               <input
+                    type="text"
+                    placeholder="Enter the task description"
+                    className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+               />
+               <button
+                    onClick={() => {
+                         if (!title.trim() || !description.trim())
+                              return alert('Preencha todos os campos');
+                         onAddTaskSubmit(title, description);
+                         setTitle('');
+                         setDescription('');
+                    }}
+                    className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+               >
+                    Add
+               </button>
           </div>
      );
 }
